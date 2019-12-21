af0b873139970e334361e048bd56224597c7ccb8
gmork2 <gmork.02@gmail.com>
2019-12-21 09:11:09 +0100

    Added a basic group model admin

--- hierarchy/admin.py
+++ hierarchy/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from django import forms
 
-# Register your models here.
+from .models import MPTTGroup
+
+
+class MPTTGroupAdminForm(forms.ModelForm):
+    class Meta:
+        model = MPTTGroup
+        fields = '__all__'
+
+
+class MPTTGroupAdmin(admin.ModelAdmin):
+    form = MPTTGroupAdminForm
+    list_display = ('group',)
+    search_fields = ('group',)
+    fieldsets = (
+        (None, {
+            'fields': ('parent', 'group',)
+        }),
+    )
+
+
+admin.site.register(MPTTGroup, MPTTGroupAdmin)
