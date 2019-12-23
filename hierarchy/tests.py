46fbc55eb6a1d0f7ef00080649e66dadd243f456
gmork2 <gmork.02@gmail.com>
2019-12-23 19:07:07 +0100

    Modified MPTTGroupTestsCase tests

--- hierarchy/tests.py
+++ hierarchy/tests.py
@@ -1,3 +1,5 @@
+from unittest import skip
+
 from django.core.exceptions import ValidationError
 from django.test import TestCase
 from django.contrib.auth.models import Group, Permission
@@ -10,34 +12,32 @@ class MPTTGroupTestCase(TestCase):
         """
 
         """
-        group_names = ('etc', 'usr', 'local', 'share', 'man',)
+        self.group_names = ('/', 'etc', 'usr', 'local', 'share', 'man',)
 
-        groups = [Group(name=n) for n in group_names]
+        groups = [Group(name=n) for n in self.group_names]
         Group.objects.bulk_create(groups)
-        for node in [MPTTGroup(group=g) for g in Group.objects.all()]:
-            node.save()
+
+        # for node in [MPTTGroup(group=g) for g in Group.objects.all()]:
+        #     node.save()
+        self.root = MPTTGroup(group=Group.objects.get(name=self.group_names[0]))
+        self.root.save()
 
     def test_circular_reference(self):
         """
         Prevent simple circular reference.
         """
-        usr = MPTTGroup.objects.get(group=Group.objects.get(name="usr"))
-        local = MPTTGroup.objects.get(group=Group.objects.get(name="local"))
-        local.parent = usr
-        local.save()
+        local = Group.objects.get(name=self.group_names[3])
+        node = MPTTGroup.objects.create(group=local, parent=self.root)
+        self.root.parent = node
 
-        usr.parent = local
-        # usr = MPTTGroup.objects.get(group=Group.objects.get(name="usr"))
-        # print(usr.get_descendants().values_list('id', flat=True))
-
-        self.assertRaises(ValidationError, usr.save)
+        self.assertRaises(ValidationError, self.root.clean)
 
     def test_set_parent_itself(self):
         """
         Prevent set parent to itself.
         """
-        group = Group.objects.get(name="etc")
-        etc = MPTTGroup.objects.get(group=group)
-        etc.parent = etc
+        local = Group.objects.get(name=self.group_names[3])
+        node = MPTTGroup.objects.create(group=local)
+        node.parent = node
 
-        self.assertRaises(ValidationError, etc.save)
+        self.assertRaises(ValidationError, node.clean)
