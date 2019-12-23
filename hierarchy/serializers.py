c2749c29b8e58fb775b441a049aa237c37273e47
gmork2 <gmork.02@gmail.com>
2019-12-23 19:33:57 +0100

    Set a recursive children field to MPTTGroupSerializer

--- hierarchy/serializers.py
+++ hierarchy/serializers.py
@@ -1,8 +1,37 @@
+from django.core.exceptions import ObjectDoesNotExist
+
 from rest_framework import serializers
+
 from .models import MPTTGroup
 
 
+class RecursiveField(serializers.BaseSerializer):
+    """
+    Create instance for serializer parent and returns serialized
+    data.
+    """
+    def to_representation(self, value):
+        parent_serializer = self.parent.parent.__class__
+        serializer = parent_serializer(value, context=self.context)
+        return serializer.data
+
+    def to_internal_value(self, data):
+        parent_serializer = self.parent.parent.__class__
+        model_class = parent_serializer.Meta.model
+        try:
+            instance = model_class.objects.get(pk=data)
+        except ObjectDoesNotExist:
+            raise serializers.ValidationError(
+                "Object {0} not found".format(
+                    model_class().__class__.__name__
+                )
+            )
+        return instance
+
+
 class MPTTGroupSerializer(serializers.ModelSerializer):
+    children = RecursiveField(many=True, required=False)
+
     class Meta:
         model = MPTTGroup
-        fields = ("id", "group",)
+        fields = ("id", "group", "parent", "children")
