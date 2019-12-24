b1535e9bb2e2663c30395d8ea0b4093b5d2e9ab0
gmork2 <gmork.02@gmail.com>
2019-12-24 17:26:11 +0100

    Removed comments in tests

--- hierarchy/tests.py
+++ hierarchy/tests.py
@@ -13,12 +13,9 @@ class MPTTGroupTestCase(TestCase):
 
         """
         self.group_names = ('/', 'etc', 'usr', 'local', 'share', 'man',)
-
         groups = [Group(name=n) for n in self.group_names]
         Group.objects.bulk_create(groups)
 
-        # for node in [MPTTGroup(group=g) for g in Group.objects.all()]:
-        #     node.save()
         self.root = MPTTGroup(group=Group.objects.get(name=self.group_names[0]))
         self.root.save()
 
