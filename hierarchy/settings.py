3f1cd777cb502f056ea177c13b55ceb7782ad637
gmork2 <gmork.02@gmail.com>
2019-12-25 00:28:25 +0100

    Added mptt methods list to local settings

--- hierarchy/settings.py
+++ hierarchy/settings.py
@@ -1,5 +1,9 @@
 from django.conf import settings
 
 
+MPTT_METHODS_LIST = (
+    'get_ancestors', 'get_children', 'get_descendants', 'get_family', 'get_next_sibling',
+    'get_previous_sibling', 'get_root', 'get_siblings',
+)
 HIERARCHY_MAX_CHILDREN = getattr(settings, 'HIERARCHY_MAX_CHILDREN', None)
-HIERARCHY_MPTT_METHOD = getattr(settings, 'HIERARCHY_MPTT_METHOD', 'get_descendants')
+HIERARCHY_MPTT_METHOD = getattr(settings, 'HIERARCHY_MPTT_METHOD', MPTT_METHODS_LIST[2])
