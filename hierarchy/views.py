a153f73dedbbdad798e21dbd94af97816367c59e
gmork2 <gmork.02@gmail.com>
2019-12-29 23:25:36 +0100

    Set permission class in MPTTGroupViewSet

--- hierarchy/views.py
+++ hierarchy/views.py
@@ -1,9 +1,11 @@
 from rest_framework import viewsets
+from rest_framework.permissions import DjangoModelPermissions
 
 from .models import MPTTGroup
 from .serializers import MPTTGroupSerializer
 
 
 class MPTTGroupViewSet(viewsets.ModelViewSet):
+    permission_classes = [DjangoModelPermissions]
     queryset = MPTTGroup.objects.filter(parent=None)
     serializer_class = MPTTGroupSerializer
